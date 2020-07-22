52100b79d8e80830bf3088613e7a00fab35085fa
Anup Baidya <anup.baidya@goomo.com>
2020-07-22 22:15:58 +0530

    email validation delete company zip code identification

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,30 @@
 class Company < ApplicationRecord
+  EMAIL_REGEX = /\w+@getmainstreet.com/.freeze
+
   has_rich_text :description
+
+   validate :validate_email
+
+   before_save :set_city_state_using_zip_code
+
+
+   def set_city_state_using_zip_code
+   	zip_code = ZipCodes.identify(self.zip_code)
+   	if zip_code.present?
+   		self.city = zip_code[:city]
+   		self.state = zip_code[:state_code]
+   	end
+   end
+
+
+  private
+
+  def validate_email
+  	if email.present?
+  	 self.errors.add(:email,"Email should only be a @getmainstreet.com domain") unless email.match(EMAIL_REGEX)
+  	 	
+  	end
+  end
 
 
 end
